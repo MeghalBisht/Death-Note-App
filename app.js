b48411ebf0420f26d2b17385089dd28fac4176fb
Meghal <mailtomeghu25@gmail.com>
2020-06-13 07:52:18 +0530

    heroku deploy

--- app.js
+++ app.js
@@ -2,13 +2,44 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const cors = require('cors')
 const path = require('path')
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+var ObjectID = require('mongoose').Types.ObjectId
 require('./db')
 require('dotenv').config();
-const router = require('./Routes/message')
 
 const app = express();
 
 app.use(cors())
+
+
+router.get('/users', (req, res) => {
+
+    User.find((err, users) => {
+        if (!err)
+            return res.status(200).send(users)
+        else
+            return res.status(422).send({ message: "Something went wrong" })
+    })
+
+})
+
+router.get('/:id/:name', (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send(`no record with given id: ${req.params.id}`)
+    else {
+        User.findById(req.params.id)
+            .then(user => {
+                return res.status(200).send(user)
+            })
+            .catch(err => {
+                return res.status(422).send(err)
+            })
+    }
+})
+
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -16,6 +47,24 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
+
+
+router.post('/create', (req, res) => {
+    const { name, message } = req.body;
+    if (!message || !name)
+        return res.status(422).json({ error: "All fields are required" })
+    const newUser = new User({
+        name,
+        message
+    })
+
+    newUser.save()
+        .then(user =>
+            res.status(200).json({ message: "received your message!", user }))
+        .catch(err => {
+            res.status(422).json({ message: "Something went wrong!" })
+        })
+})
 
 app.use(express.json())
 app.use('/', router)
