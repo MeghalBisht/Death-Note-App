db76d88ba8c409cca59620e815ddeb1493350585
Meghal <mailtomeghu25@gmail.com>
2020-06-13 20:26:39 +0530

    deployed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/', router)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
